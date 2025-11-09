c28b7b6b18e08e7d366a7f7773e04849d6d2049a
新元noname <13257656+xinyuanwm@user.noreply.gitee.com>
2025-11-09 20:33:52 +0800

    feat(number): 添加 clamp 函数及其类型定义

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export * from './src/array.js';
 export * from './src/event.js';
 export * from './src/guard.js';
 export * from './src/missingData.js';
+export * from './src/number.js';
 export * from './src/object.js';
 export * from './src/random.js';
 export * from './src/string.js';
---
+++ src/number.js
@@ -0,0 +1,9 @@
+import { throwIfIsNotComparableNumber } from "./guard.js"
+
+
+export function clamp(value, min, max) {
+    throwIfIsNotComparableNumber(value, "value");
+    throwIfIsNotComparableNumber(min, "min");
+    throwIfIsNotComparableNumber(max, "max");
+    return Math.max(Math.min(value, max), min);
+}
